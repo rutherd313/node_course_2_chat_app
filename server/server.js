52ea7365d446dd280f27dc8500ee2c268e4fe379
John Dueno <djohnruther@yahoo.com>
2018-07-19 14:11:53 -0500

    Init commit

---
+++ server/server.js
@@ -0,0 +1,20 @@
+//Root of node app
+const path = require('path');
+const publicPath = path.join(__dirname, '../public');
+const express = require('express');
+// console.log(__dirname + '/../public'); //old way
+// console.log(publicPath); //new way 
+
+//Configure for Heroku
+const port = process.env.PORT || 3000;
+
+//app var to config express 
+var app = express();
+
+//configs static middleware, serves public folder
+app.use(express.static(publicPath));
+
+//starts sever on localhost
+app.listen(3000, () => {
+	console.log(`Server is up on port ${port}`);
+})
